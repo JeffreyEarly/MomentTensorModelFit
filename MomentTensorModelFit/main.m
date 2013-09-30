54bddf5b89cb1369ba6023983298aea0d7e2a077
Jeffrey J. Early <jearly@nwra.com>
2013-09-30 15:11:39 -0700

    Initial Commit

---
+++ MomentTensorModelFit/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MomentTensorModelFit
+//
+//  Created by Jeffrey J. Early on 9/30/13.
+//  Copyright (c) 2013 Jeffrey J. Early. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
